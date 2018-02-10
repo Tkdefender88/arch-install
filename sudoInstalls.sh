9938ea6c671d3f29319605124cafb6c349fa22bf
Tkdefender <tkdefender88@gmail.com>
2018-02-10 00:03:11 -0700

    Update sudoInstalls.sh

--- sudoInstalls.sh
+++ sudoInstalls.sh
@@ -27,7 +27,7 @@ useradd -m -g wheel -s /usr/bin/zsh tbrooks
 wget https://raw.githubusercontent.com/trevorlbrooks/arch-install/master/tempSudo
 cp tempSudo /etc/sudoers.d/tempSudo
 chmod 0440 /etc/sudoers.d/tempSudo
-su tbrooks -c sh -c "$(curl -fsSL https://raw.githubusercontent.com/trevorlbrooks/arch-install/master/userInstalls.sh)"
+su tbrooks -c sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tkdefender88/arch-install/master/userInstalls.sh)"
 rm /etc/sudoers.d/tempSudo
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/wheel
 chmod 0440 /etc/sudoers.d/wheel
